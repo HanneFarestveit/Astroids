dad1509ecc96d986c1156bba3c519963e5b662fe
Hanne Minken Farestveit <hannemfa@ifi.uio.no>
2025-01-07 18:19:35 +0100

    D: made the groups updatable and drawable to contain relevant game objects

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def main():
     
     game_clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
@@ -21,11 +25,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        player.update(dt)
         
         screen.fill("black")
-        player.draw(screen)
+        
+        for obj in updatable: 
+            obj.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
         
         lapsed_time = game_clock.tick(60)
