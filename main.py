5a3d3d5e3a385d72709ba484e436b78f0c5712d5
Hanne Minken Farestveit <hannemfa@ifi.uio.no>
2025-01-07 17:18:24 +0100

    added clock object and dt variable

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    game_clock = pygame.time.Clock()
+    dt = 0
+    
     #Game loop:
     while True:
         for event in pygame.event.get():
@@ -18,6 +21,9 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        
+        lapsed_time = game_clock.tick(60)
+        dt = lapsed_time / 1000
     
 
 if __name__ == "__main__":
